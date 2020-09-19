e291525f984e48fb6c0d3b34d77df6098db2130a
rosegriffin752 <60629645+rosegriffin752@users.noreply.github.com>
2020-09-19 14:46:56 -0400

    Update GradeBookTester.java

--- JUnit Lab/GradeBookTester.java
+++ JUnit Lab/GradeBookTester.java
@@ -3,7 +3,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+/**
+ * Tests GradeBook class
+ * @author Rose Griffin
+ *
+ */
 class GradeBookTester {
 	GradeBook g1;
 	GradeBook g2;
