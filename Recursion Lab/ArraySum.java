d91a8edb739c5ae1db74cde67c3b9bda0fb5414b
rosegriffin752 <60629645+rosegriffin752@users.noreply.github.com>
2020-09-30 20:00:01 -0400

    Add files via upload

---
+++ Recursion Lab/ArraySum.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Rose Griffin
+ *
+ */
+public class ArraySum {
+
+	public int sumOfArray (Integer[] a, int index) {
+		if (index >= 0) 
+			return a[index] + sumOfArray(a, --index);
+		else 
+			return 0;
+	}
+
+}
